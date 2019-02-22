17775c5595c3b8dddfb7ec63e1072ac1e8903309
Kirill <8338050@gmail.com>
2019-02-22 11:29:57 +0300

    whatsoever

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,20 @@
-import com.rometools.rome.feed.synd.SyndFeed;
-import com.rometools.rome.io.SyndFeedInput;
-import com.rometools.rome.io.XmlReader;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-
-import java.io.InputStream;
-
 public class Main {
     public static void main(String[] args) {
-//        String url = "https://www.nasa.gov/rss/dyn/johnon_news.rss";
-        String urlNew = "https://www.jpl.nasa.gov/multimedia/rss/news.xml";
-        try (CloseableHttpClient client = HttpClients.createMinimal()) {
-            HttpUriRequest request = new HttpGet(urlNew);
-            try (CloseableHttpResponse response = client.execute(request);
-                 InputStream stream = response.getEntity().getContent()) {
-                SyndFeedInput input = new SyndFeedInput();
-                SyndFeed feed = input.build(new XmlReader(stream));
+        Console cli = new Console();
+        cli.run();
 
-                System.out.println(feed.getEntries());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
+
+
+
+
+
+
+//        org.apache.log4j.BasicConfigurator.configure();
+//        String urlNew = "https://www.jpl.nasa.gov/multimedia/rss/news.xml";
+//        RssParser parser = new RssParser(urlNew, 1);
+//
+//        parser.dumb();
+
     }
 }
--- src/main/java/RssParser.java
+++ src/main/java/RssParser.java
@@ -1,4 +1,16 @@
 
+import com.rometools.rome.feed.synd.SyndEntry;
+import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.io.SyndFeedInput;
+import com.rometools.rome.io.XmlReader;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.InputStream;
+import java.util.List;
 
 public class RssParser implements Runnable {
     private String URL;
@@ -10,6 +22,23 @@ public class RssParser implements Runnable {
     }
 
 
+    public void dumb() {
+        String urlNew = "https://www.jpl.nasa.gov/multimedia/rss/news.xml";
+        try (CloseableHttpClient client = HttpClients.createMinimal()) {
+            HttpUriRequest request = new HttpGet(urlNew);
+            try (CloseableHttpResponse response = client.execute(request);
+                 InputStream stream = response.getEntity().getContent()) {
+                SyndFeedInput input = new SyndFeedInput();
+                SyndFeed feed = input.build(new XmlReader(stream));
+                List<SyndEntry> entries = feed.getEntries();
+                SyndEntry entry = entries.get(0);
+
+                System.out.println(entry.getTitle());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void run() {
